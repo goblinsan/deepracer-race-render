91395725680d3b58efffe40e04c62ca8e3501102
goblinsan <coghlanjames@yahoo.com>
2020-09-06 02:43:33 -0400

    convert markers to animation frames references at 24 fps

--- camera_activation.py
+++ camera_activation.py
@@ -26,7 +26,7 @@ def get_best_car():
             best_time = car_time
             best_car_csv = i['plot_file']
 
-    return best_car_csv
+    return best_car_csv, best_time
 
 
 def get_race_coords(csv_filepath):
@@ -45,10 +45,36 @@ def is_coord_in_zone(coord, zone):
            and point_in_range(coord[1], zone['min_y'], zone['max_y'])
 
 
+def get_coord_markers(coords, zones):
+    marker_list = []
+    i = 0
+    for z, zone in enumerate(zones):
+        if z % 6 == 0:
+            while is_coord_in_zone(coords[i], zone):
+                i += 1
+            marker_list.append(i)
+        else:
+            while not is_coord_in_zone(coords[i], zone):
+                i += 1
+            marker_list.append(i)
+            while is_coord_in_zone(coords[i], zone):
+                i += 1
+
+    return marker_list
+
+
+def convert_markers_to_frames(coord_markers, total_time, total_points):
+    frames_per_sec = 24
+    total_frames = total_time * frames_per_sec
+    frames_per_point = total_frames / total_points
+
+    return [round(coord_index * frames_per_point) for coord_index in coord_markers]
+
+
 if __name__ == '__main__':
 
-    print(get_best_car())
-    coords = get_race_coords(f"data_prep/{get_best_car()}")
+    best_csv, best_time = get_best_car()
+    coords = get_race_coords(f"data_prep/{best_csv}")
 
     # max and min of activation zones in blend file - see get_max_min.py
     zone_1 = create_zone(2.2383158206939697, 3.514540433883667, 0.028154075145721436, 1.2572650909423828)
@@ -57,37 +83,13 @@ if __name__ == '__main__':
     zone_4 = create_zone(5.544686794281006, 6.403702259063721, 2.0969109535217285, 3.326022148132324)
     zone_5 = create_zone(0.31292879581451416, 3.658247947692871, 3.757744073867798, 4.9868550300598145)
     zone_6 = create_zone(0.42658960819244385, 1.9064496755599976, 0.8830092549324036, 2.189504861831665)
+    zone_list = [zone_1, zone_2, zone_3, zone_4, zone_5, zone_6]
 
-    coord_markers = []
-    i = 0
-    while is_coord_in_zone(coords[i], zone_1):
-        i += 1
-    coord_markers.append(i)
-    while not is_coord_in_zone(coords[i], zone_2):
-        i += 1
-    coord_markers.append(i)
-    while is_coord_in_zone(coords[i], zone_2):
-        i += 1
-    while not is_coord_in_zone(coords[i], zone_3):
-        i += 1
-    coord_markers.append(i)
-    while is_coord_in_zone(coords[i], zone_3):
-        i += 1
-    while not is_coord_in_zone(coords[i], zone_4):
-        i += 1
-    coord_markers.append(i)
-    while is_coord_in_zone(coords[i], zone_4):
-        i += 1
-    while not is_coord_in_zone(coords[i], zone_5):
-        i += 1
-    coord_markers.append(i)
-    while is_coord_in_zone(coords[i], zone_5):
-        i += 1
-    while not is_coord_in_zone(coords[i], zone_6):
-        i += 1
-    coord_markers.append(i)
-
+    coord_markers = get_coord_markers(coords, zone_list + zone_list + zone_list)
     print(coord_markers)
+
+    frame_markers = convert_markers_to_frames(coord_markers, best_time, len(coords))
+    print(frame_markers)
 
 # total_time * 24 / num_points = frames per point
 
